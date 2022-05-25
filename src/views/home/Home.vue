dd62d58220e5c2ef95cf4b888d1b59df83efde63
Yangguangwa <1076801853@qq.com>
2022-05-25 16:43:57 +0800

    主页开发

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,15 +1,24 @@
 <template>
   <div id="home">
     <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+    <home-swiper :banners="banners"/>
+    <recommend-view :recommends="recommends"></recommend-view>
   </div>
 </template>
 
 <script>
 import NavBar from 'components/common/navbar/NavBar'
+import HomeSwiper from './HomeComps/HomeSwiper.vue'
+import recommendView from './HomeComps/RecommendView.vue'
+
 import {getHomeMultidata} from 'network/home'
+
+
 export default {
   components:{
-    NavBar
+    NavBar,
+    HomeSwiper,
+    recommendView
   },
   data(){
     return{
@@ -19,6 +28,7 @@ export default {
   },
   created(){
     getHomeMultidata().then(res=>{
+
       this.banners=res.data.banner.list;
       this.recommends=res.data.recommend.list;
     })
@@ -31,4 +41,7 @@ export default {
   background-color: var(--color-tint);
   color: #fff;
 }
+
+
+
 </style>
---
+++ src/views/home/HomeComps/HomeSwiper.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+      <slider class="slider" animation="normal" height="30vh">
+      <slider-item v-for="(i, index) in banners" :key="index">
+        <a :href="i.link">
+          <img :src="i.image">
+        </a>
+        
+      </slider-item>
+    </slider>
+  </div>
+</template>
+
+<script>
+import EasySlider from 'vue-easy-slider'
+export default {
+    props:{
+        banners:{
+            type:Array,
+            default(){
+                return []
+            }
+        }
+        
+    }
+}
+</script>
+
+<style>
+.slider img{
+  position: fixed;
+  width: 100vw;
+  height: 30vh;
+}
+</style>
